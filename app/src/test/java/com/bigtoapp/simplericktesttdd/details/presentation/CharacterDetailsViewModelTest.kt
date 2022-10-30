3851ef7fa699a68277774aa9b3b654f7782d722a
AntonEsipow <esipow.anton99@gmail.com>
2022-10-30 19:29:53 +0300

    mapper test and base test

--- app/src/test/java/com/bigtoapp/simplericktesttdd/details/presentation/CharacterDetailsViewModelTest.kt
+++ app/src/test/java/com/bigtoapp/simplericktesttdd/details/presentation/CharacterDetailsViewModelTest.kt
@@ -14,28 +14,12 @@ import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 
-class CharacterDetailsViewModelTest {
+class CharacterDetailsViewModelTest: BaseTest() {
 
 
     private lateinit var viewModel: CharacterDetailsViewModel
     private lateinit var interactor: TestCharacterDetailsInteractor
     private lateinit var communications: TestCharacterDetailsCommunication
-
-    private val detailsDomain = CharacterDetailsDomain(
-        id = "42",
-        name = "Morty Smith",
-        status = "Alive",
-        species = "Human",
-        gender = "male"
-    )
-
-    private val detailsUi = CharacterDetailsUi(
-        id = "42",
-        name = "Morty Smith",
-        status = "Alive",
-        species = "Human",
-        gender = "male"
-    )
 
     @Before
     fun setUp() {
@@ -71,7 +55,6 @@ class CharacterDetailsViewModelTest {
          assertEquals(View.GONE, communications.progressCalledList[1])
 
          assertEquals(1, communications.stateCalledList.size)
-//         assertEquals(true, communications.stateCalledList[0] is UiState.Success)
          assertEquals(UiState.Success, communications.stateCalledList[0] )
 
          assertEquals(1, communications.timesShowDetails)
@@ -111,31 +94,6 @@ class CharacterDetailsViewModelTest {
             detailsCalledItem.add(result)
             return result
         }
-    }
-
-    private class TestCharacterDetailsCommunication: CharacterDetailsCommunications {
-
-        val progressCalledList = mutableListOf<Int>()
-        val stateCalledList = mutableListOf<UiState>()
-        var timesShowDetails = 0
-        val detailsUiList = mutableListOf<CharacterDetailsUi>()
-
-        override fun showProgress(show: Int) {
-            progressCalledList.add(show)
-        }
-
-        override fun showState(uiState: UiState) {
-            stateCalledList.add(uiState)
-        }
-
-        override fun showDetails(details: CharacterDetailsUi) {
-            detailsUiList.add(details)
-            timesShowDetails++
-        }
-
-        override fun observeProgress(owner: LifecycleOwner, observer: Observer<Int>) = Unit
-        override fun observeState(owner: LifecycleOwner, observer: Observer<UiState>) = Unit
-        override fun observeDetails(owner: LifecycleOwner, observer: Observer<CharacterDetailsUi>) = Unit
     }
 
     private class TestDispatchersList(
